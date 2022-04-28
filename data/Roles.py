1da19022ee6065a33610cdaea2bef57361709c34
Андрей <tatarinovandry@mail.ru>
2022-04-28 22:13:56 +0300

    файл для работы с таблицей roles из бд

---
+++ data/Roles.py
@@ -0,0 +1,14 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Roles(SqlAlchemyBase):
+    __tablename__ = 'roles'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    id_server = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    id_owner = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    banned_role = sqlalchemy.Column(sqlalchemy.String)
+    admitted_users = sqlalchemy.Column(sqlalchemy.String)
+
